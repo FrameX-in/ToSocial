b902c1e0d34aad2ce563255a02eddee54adec697
Aniket <aniketsudke03@gmail.com>
2024-02-10 12:15:14 +0530

    Nav Completed

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -1,43 +1,64 @@
 "use client"
 import React, { useState } from 'react';
 import { LinkIcon, Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid'
+import Link from 'next/link';
 
 const Header = () => {
     let Links =[
-        {name:"HOME",link:"/"},
-        {name:"SERVICE",link:"/"},
-        {name:"ABOUT",link:"/"},
-        {name:"CONTACT",link:"/"},
+        {name:"Home",link:"/"},
+        {name:"Pricing",link:"/pricing"},
+        {name:"About",link:"/about"},
+        {name:"Contact",link:"/contact"},
       ];
       let [open, setOpen] =useState(false);
 
     return (
-        <div className='shadow-md w-full fixed top-0 left-0'>
-           <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
-            {/* logo section */}
-            <div className='font-bold text-2xl cursor-pointer flex items-center gap-1'>
-                <LinkIcon className='w-7 h-7 text-blue-600'/>
-                <span>ToSocial</span>
-            </div>
-            {/* Menu icon */}
-            <div onClick={()=>setOpen(!open)} className='absolute right-8 top-6 cursor-pointer md:hidden w-7 h-7'>
-                {
-                    open ? <XMarkIcon/> : <Bars3BottomRightIcon />
+    
+    <nav
+    class="relative flex w-full items-center justify-between bg-[hsla(0,0%,100%,0.55)] py-2  shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] dark:bg-neutral-800 dark:shadow-black/30  lg:flex-wrap lg:justify-start"
+    data-te-navbar-ref>
+    <div class="flex w-full flex-wrap items-center justify-between px-6">
+      <div class="flex items-center">
+      <div onClick={()=>setOpen(!open)} className='absolute right-8 top-2 cursor-pointer md:hidden w-7 h-7'>
+            {
+                 open ? <XMarkIcon/> : <Bars3BottomRightIcon />
                 }
-            </div>
-            {/* linke items */}
-            <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-12' : 'top-[-490px]'}`}>
-                {
+             </div>
+
+        <a class="text-primary dark:text-primary-400" href="#!">
+          <span class="[&>svg]:ml-2 [&>svg]:mr-3 [&>svg]:h-6 [&>svg]:w-6 [&>svg]:lg:ml-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+            </svg>
+          </span>
+        </a>
+      </div>
+
+      <div class={`!visible ${!open ? 'hidden' : null}  flex-grow basis-[100%] items-center lg:!flex lg:basis-auto `}
+         data-te-collapse-item>
+        <ul class="mr-auto lg:flex lg:flex-row" data-te-navbar-nav-ref>
+        {
                     Links.map((link) => (
-                    <li className='md:ml-8 md:my-0 my-7 font-semibold'>
-                        <a href={link.link} className='text-gray-800 hover:text-blue-400 duration-500'>{link.name}</a>
-                    </li>))
+                        <li data-te-nav-item-ref>
+                        <Link class="block py-2 pr-2 text-neutral-500 transition duration-150 ease-in-out hover:text-neutral-600 focus:text-neutral-600 disabled:text-black/30 dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 dark:disabled:text-white/30 lg:px-2 [&.active]:text-black/80 dark:[&.active]:text-white/80"
+                          href={link.link} data-te-nav-link-ref data-te-ripple-init data-te-ripple-color="light"
+                          disabled>{link.name}</Link>
+                      </li>))
                 }
-                <button className='btn bg-blue-600 text-white md:ml-8 font-semibold px-3 py-1 rounded duration-500 md:static'>Get Started</button>
-            </ul>
-            {/* button */}
-           </div>
-        </div>
+          
+         
+        </ul>
+        <div className='flex gap-2 md:ml-10 sm:mt-2'>
+               <Link href={"/login"} className='btn bg-blue-600 text-white md:ml-4 font-semibold px-2 py-1 rounded duration-500 md:static '>Login</Link>
+                <Link href={"/register"} className='btn bg-blue-600 text-white md:ml-4 font-semibold px-2 py-1 rounded duration-500 md:static '>Register</Link>
+               </div>
+      </div>
+
+      
+    </div>
+  </nav> 
     );
 };
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
 import Header from "./_components/Header";
+import Heros from "./_components/Heros";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      {/* <Header /> */}
+      <Heros />
+      {/* <h1 className="absolute">HEllO</h1> */}
+    </>
+  );
 }
